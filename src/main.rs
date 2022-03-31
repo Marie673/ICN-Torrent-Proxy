e258e75099d6faa73076b6a0c54bcbc0adf7a8dd
OKAZAKI Kohei <okazaki@net.info.hiroshima-cu.ac.jp>
2022-03-31 11:49:32 +0900

    Changes

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ use parse_torrent::Torrent;
 
 fn main() {
     let test = Torrent::from_file("/mnt/usbhdd3/Okazaki/Torrent_Proxy/test/ubuntu-20.04.3-desktop-amd64.iso.torrent").unwrap();
-    println!("test: {:?}", test{announce});
+    println!("test: {:?}", test{info});
 }
