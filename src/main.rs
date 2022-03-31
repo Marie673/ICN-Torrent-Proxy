09d5f188fa73c0344fb46f46de2ee05b2a2c7887
OKAZAKI Kohei <okazaki@net.info.hiroshima-cu.ac.jp>
2022-03-31 10:58:41 +0900

    Changes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-mod bittorrent;
-use bittorrent::block;
+use parse_torrent::Torrent;
 
 fn main() {
-    bittorrent::ping();
-    block::echo();
+    let test = Torrent::from_file("/mnt/usbhdd3/Okazaki/Torrent_Proxy/test/ubuntu-20.04.3-desktop-amd64.iso.torrent").unwrap();
+    println!("test: {:?}", test);
 }
